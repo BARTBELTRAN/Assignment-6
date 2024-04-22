84d302e6081ad256bec68a9967050d49dbf985b6
BARTBELTRAN <84153478+BARTBELTRAN@users.noreply.github.com>
2024-04-21 20:32:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+let textarea = document.createElement("textarea");
+textarea.setAttribute("cols", "50");
+textarea.setAttribute("rows", "5");
+textarea.setAttribute("placeholder", "Hello");
+
+document.body.appendChild(textarea);
+
+const submitButton = document.createElement("button");
+submitButton.textContent = "Submit";
+submitButton.type = "submit";
+
+submitButton.addEventListener("click", function(event) {
+	const inputText = textarea.value;
+  
+  const tokens = inputText.split(/\s+/);
+  
+  const frequencyTable = {};
+  
+  tokens.forEach(token => {
+  	if (frequencyTable[token]) {
+    	frequencyTable[token]++;
+    }
+    else {
+    	frequencyTable[token] = 1;
+    }
+  });
+  console.log("Frequency Table:", frequencyTable);
+});
+
+document.body.appendChild(submitButton);
