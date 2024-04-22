01119d8be4a7fbc8ed3d691a6ddc9261fc7191e3
BARTBELTRAN <84153478+BARTBELTRAN@users.noreply.github.com>
2024-04-21 21:48:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let textarea = document.createElement("textarea");
 textarea.setAttribute("cols", "50");
 textarea.setAttribute("rows", "5");
@@ -26,6 +25,40 @@ submitButton.addEventListener("click", function(event) {
     }
   });
   console.log("Frequency Table:", frequencyTable);
+  
+  //sort
+  const sortedTable = Object.entries(frequencyTable)
+  .sort((a, b) => b[1] - a[1]);
+  
+  //top five words
+  const topFive = sortedTable.slice(0, 5);
+  
+  //output statement
+  const outputDiv = document.createElement("div");
+  outputDiv.textContent = "Top 5 most frequent words are:";
+  document.body.appendChild(outputDiv);
+  
+  //HTML table
+  const table = document.createElement("table");
+  const tableHeader = table.createTHead();
+  const headerRow = tableHeader.insertRow();
+  const header1 = document. createElement("th");
+  const header2 = document.createElement("th");
+  header1.textContent = "word_name";
+  header2.textContent = "count";
+  headerRow.appendChild(header1);
+  headerRow.appendChild(header2);
+  
+  //append to table
+  topFive.forEach(item => {
+  	const row = table.insertRow();
+    const cell1 = row.insertCell();
+    const cell2 = row.insertCell();
+    cell1.textContent = item[0];
+    cell2.textContent = item[1];
+  });
+  
+  document.body.appendChild(table);
 });
 
 document.body.appendChild(submitButton);
